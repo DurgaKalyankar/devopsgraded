aac50746d1dc839b9c4bd4a1792a95b27a7902f4
kalyankarDurga <kalyankar.durga@gmail.com>
2021-06-07 21:38:17 +0530

    initial commit

--- src/main/java/com/example/demo/DevopsgradedApplication.java
+++ src/main/java/com/example/demo/DevopsgradedApplication.java
@@ -8,7 +8,7 @@ public class DevopsgradedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsgradedApplication.class, args);
-		System.out.println("devops");
+		System.out.println("devops graded assignment");
 	}
 
 }
